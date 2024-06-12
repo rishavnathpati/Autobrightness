a896ae9fe30d1b86a970fdd526ef8e835a031df5
Rishav Nath Pati <patirishavnath@gmail.com>
2024-06-12 10:47:50 +0530

    refactor: Refactor AutoBrightnessApp to separate TrayIconManager and WebcamController

--- brightness.py
+++ brightness.py
@@ -20,32 +20,79 @@ from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QImage, QPixmap, QPainter, QFont, QIcon
 
 
-class AutoBrightnessApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.timer = None
-        self.initUI()
-        self.initTrayIcon()
-
-    def initTrayIcon(self):
-        self.tray_icon = QSystemTrayIcon(self)
+class TrayIconManager:
+    def __init__(self, parent):
+        self.tray_icon = QSystemTrayIcon(parent)
         self.tray_icon.setIcon(QIcon("icon.png"))  # Replace with your icon file
         self.tray_icon.setVisible(True)
-
-        self.tray_menu = QMenu(self)
-        self.show_action = QAction("Show", self)
-        self.show_action.triggered.connect(self.show)
-        self.hide_action = QAction("Hide", self)
-        self.hide_action.triggered.connect(self.hide)
-        self.quit_action = QAction("Quit", self)
+        
+        self.tray_menu = QMenu(parent)
+        self.show_action = QAction("Show", parent)
+        self.show_action.triggered.connect(parent.show)
+        self.hide_action = QAction("Hide", parent)
+        self.hide_action.triggered.connect(parent.hide)
+        self.quit_action = QAction("Quit", parent)
         self.quit_action.triggered.connect(QApplication.quit)
+        
         self.tray_menu.addAction(self.show_action)
         self.tray_menu.addAction(self.hide_action)
         self.tray_menu.addSeparator()
         self.tray_menu.addAction(self.quit_action)
 
         self.tray_icon.setContextMenu(self.tray_menu)
-        self.tray_icon.activated.connect(self.onTrayIconActivated)
+        self.tray_icon.activated.connect(parent.onTrayIconActivated)
+
+
+class WebcamController:
+    def __init__(self, brightness_slider, exposure_slider):
+        self.cap = None
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.brightness_slider = brightness_slider
+        self.exposure_slider = exposure_slider
+        self.current_brightness = None
+
+    def start_webcam(self):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.5)
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, self.exposure_slider.value())
+        self.timer.start(100)
+
+    def stop_webcam(self):
+        if self.timer is not None:
+            self.timer.stop()
+        if self.cap is not None:
+            self.cap.release()
+
+    def update_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            luminance = np.mean(gray)
+            target_brightness = int(luminance / self.brightness_slider.value() * 100)
+            smoothing_factor = 0.1
+            if self.current_brightness is None:
+                self.current_brightness = target_brightness
+            else:
+                self.current_brightness = (
+                    1 - smoothing_factor
+                ) * self.current_brightness + smoothing_factor * target_brightness
+            self.set_brightness(int(self.current_brightness))
+
+    def set_brightness(self, level):
+        try:
+            wmi_interface = wmi.WMI(namespace="wmi")
+            methods = wmi_interface.WmiMonitorBrightnessMethods()[0]
+            methods.WmiSetBrightness(int(level), 0)
+        except wmi.x_wmi as e:
+            print(f"Error setting brightness: {str(e)}")
+
+
+class AutoBrightnessApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.tray_icon_manager = TrayIconManager(self)
+        self.initUI()
 
     def initUI(self):
         layout = QVBoxLayout()
@@ -100,56 +147,31 @@ class AutoBrightnessApp(QWidget):
         self.show()
 
     def start_webcam(self):
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.5)
-        self.cap.set(cv2.CAP_PROP_EXPOSURE, self.exposure_slider.value())
+        self.webcam_controller = WebcamController(self.brightness_slider, self.exposure_slider)
+        self.webcam_controller.start_webcam()
         self.timer = QTimer()
-        self.timer.timeout.connect(self.update_frame)
+        self.timer.timeout.connect(self.update_luminance_display)
         self.timer.start(100)
         self.start_button.setEnabled(False)
         self.stop_button.setEnabled(True)
 
     def stop_webcam(self):
-        if self.timer is not None:
-            self.timer.stop()
-            self.timer = None
-        if hasattr(self, "cap"):
-            self.cap.release()
+        self.webcam_controller.stop_webcam()
         self.start_button.setEnabled(True)
         self.stop_button.setEnabled(False)
 
-    def update_frame(self):
-        ret, frame = self.cap.read()
-        if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            luminance = np.mean(gray)
-            target_brightness = int(luminance / self.brightness_slider.value() * 100)
-            smoothing_factor = 0.1
-            if not hasattr(self, "current_brightness"):
-                self.current_brightness = target_brightness
-            else:
-                self.current_brightness = (
-                    1 - smoothing_factor
-                ) * self.current_brightness + smoothing_factor * target_brightness
-            self.set_brightness(int(self.current_brightness))
-
-            luminance_image = np.full((200, 200), luminance, dtype=np.uint8)
-            q_img = QImage(luminance_image.data, 200, 200, QImage.Format_Grayscale8)
-            painter = QPainter(q_img)
-            painter.setPen(Qt.black)
-            painter.setFont(QFont("Arial", 12))
-            painter.drawText(10, 30, f"Luminance: {luminance:.2f}")
-            painter.drawText(10, 50, f"Brightness: {int(self.current_brightness)}")
-            painter.end()
-            self.luminance_label.setPixmap(QPixmap.fromImage(q_img))
-
-    def set_brightness(self, level):
-        try:
-            wmi_interface = wmi.WMI(namespace="wmi")
-            methods = wmi_interface.WmiMonitorBrightnessMethods()[0]
-            methods.WmiSetBrightness(int(level), 0)
-        except wmi.x_wmi as e:
-            print(f"Error setting brightness: {str(e)}")
+    def update_luminance_display(self):
+        luminance = self.webcam_controller.current_brightness or 0
+        brightness = int(self.webcam_controller.current_brightness) if self.webcam_controller.current_brightness is not None else 0
+        luminance_image = np.full((200, 200), luminance, dtype=np.uint8)
+        q_img = QImage(luminance_image.data, 200, 200, QImage.Format_Grayscale8)
+        painter = QPainter(q_img)
+        painter.setPen(Qt.black)
+        painter.setFont(QFont("Arial", 12))
+        painter.drawText(10, 30, f"Luminance: {luminance:.2f}")
+        painter.drawText(10, 50, f"Brightness: {brightness}")
+        painter.end()
+        self.luminance_label.setPixmap(QPixmap.fromImage(q_img))
 
     def onTrayIconActivated(self, reason):
         if reason == QSystemTrayIcon.DoubleClick:
